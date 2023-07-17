6d8b7983a1c4fd57941f9df9caed085d5b65f5ae
rahulsawra98 <104886535+rahulsawra98@users.noreply.github.com>
2023-07-17 13:59:28 +0530

    include client and server redundancy numbers (#25)
    * include server and client gw redundancy number, rotation interval and make renewBefore pointer to time
    
    Signed-off-by: rahulsawra98 <rahul.sawra@aveshasystems.com>
    ---------
    
    Signed-off-by: rahulsawra98 <rahul.sawra@aveshasystems.com>

--- pkg/controller/v1alpha1/sliceconfig_types.go
+++ pkg/controller/v1alpha1/sliceconfig_types.go
@@ -42,8 +42,12 @@ type SliceConfigSpec struct {
 	//+kubebuilder:validation:Minimum=2
 	//+kubebuilder:validation:Maximum=32
 	MaxClusters int `json:"maxClusters"`
+	//+kubebuilder:validation:Minimum=30
+	//+kubebuilder:validation:Maximum=90
+	//+kubebuilder:default:=30
+	RotationInterval int `json:"rotationInterval,omitempty"`
 	// RenewBefore is used for renew now!
-	RenewBefore metav1.Time `json:"renewBefore,omitempty"`
+	RenewBefore *metav1.Time `json:"renewBefore,omitempty"`
 }
 
 // ExternalGatewayConfig is the configuration for external gateways like 'istio', etc/
